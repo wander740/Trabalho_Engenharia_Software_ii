b5c77937dc3992c6333b7b1d9fb6e272c68b02e7
Rafael Waiti G. Umetsu <rafael.umetsu@gmail.com>
2020-10-05 21:10:20 +0000

    Adicionei o cadastro, não tem verificação de em brando e o segurança da senha tá torto

--- src/App.js
+++ src/App.js
@@ -31,6 +31,94 @@ function App() {
         <br />
         <Button label="Cadastro" />
 
+				{/*----------- CADASTRO -----------
+				        <span className="p-float-label">
+          <InputText id="cadastroNome" />
+          <label htmlhtmlFor="cadastro">Nome Completo</label>
+        </span>
+        <br />
+        <span className="p-float-label">
+          <InputText id="cadastroEmail" />
+          <label htmlhtmlFor="cadastro">Email</label>
+        </span>
+        <br />
+        <span className="p-float-label">
+          <Password promptLabel="Digite uma Senha" weakLabel="Senha Fraca" mediumLabel="Senha Média" strongLabel="Senha Forte"/>
+          <label htmlhtmlFor="cadastro">Senha</label>
+        </span>
+        <br />
+        <span className="p-float-label">
+        	<InputText id="cadastroSenhaSenha" />
+          <label htmlhtmlFor="cadastro">Confirmar Senha</label>
+        </span>
+        <br />
+        <span className="p-float-label">
+						<InputMask id="cadastroCPF" mask="999.999.999-99"/>
+						<label htmlhtmlFor="cadastro">CPF</label>
+				</span>
+        <br />
+        <span className="p-float-label">
+        	<InputText id="in" />
+          <label htmlhtmlFor="cadastro">Endereço</label>
+        </span>
+        <br />
+				<span className="p-float-label">
+						<InputMask id="cadastroCelular" mask="(99) 99999-9999"/>
+						<label htmlhtmlFor="cadastro">Nº Celular</label>
+				</span>	
+        <br />
+        <Button label="Cadastrar" onClick={this}/>
+        <br />
+        <Button label="Voltar" onClick={this}/>
+				*/}
+
+							{/* ------ CADASTRO CONFIRMADO -------
+			
+			*/}
+
+
+			{/* ------ HOME -------
+				{IMAGEM 1}
+			  <br />
+				{IMAGEM 2}
+				<br />
+				<br />
+        <Button label="Fazer Pedido" onClick={this}/>
+        <br />
+        <Button label="Status do Pedido" onClick={this}/>
+        <br />
+				<br />
+        <Button label="Configuração" onClick={this}/>		
+			*/}
+
+
+			{/* ------ PEDIDO -------
+
+			*/}
+
+
+			{/* ------ PEDIDO CONCLUIDO -------
+
+			*/}
+
+
+			{/* ------ STATUS DO PEDIDO -------
+
+
+			*/}
+
+			{/* ------ CONFIGURAÇÃO -------
+
+			*/}
+
+			{/* ------ EDITAR PERFIL -------
+
+			*/}
+
+			{/* ------ EDITAR PERFIL CONCLUIDO -------
+
+			*/}
+
       </header>
     </div>
   );
